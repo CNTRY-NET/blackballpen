8a234fbfc80eba9cf656c96694ce87d460657f98
吴晨 <wuchen@zetyun.com>
2020-04-08 12:20:13 +0800

    test1

--- src/main/java/com/cntryroa/blackballpen/DemoApplication.java
+++ src/main/java/com/cntryroa/blackballpen/DemoApplication.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("haha");
         SpringApplication.run(DemoApplication.class, args);
     }
 
