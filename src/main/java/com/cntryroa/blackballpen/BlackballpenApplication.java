a183eac6e825a1a1dee7e446bb2c0309cc89ea7c
吴晨 <wuchen@zetyun.com>
2020-04-08 12:10:03 +0800

    2

---
+++ src/main/java/com/cntryroa/blackballpen/BlackballpenApplication.java
@@ -0,0 +1,13 @@
+package com.cntryroa.blackballpen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class BlackballpenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackballpenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cntryroa/blackballpen/BlackballpenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cntryroa.blackballpen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackballpenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
